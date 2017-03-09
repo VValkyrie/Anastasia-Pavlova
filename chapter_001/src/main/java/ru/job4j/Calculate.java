8db3b67888379fadd0644361f5c7efcfa5bc8241
VValkyrie <a-pavlova-1986@yandex.ru>
2017-03-09 17:48:17 +0300

    add maven

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,11 @@
+﻿package ru.job4j;
+/**
+* //package for program "Hello, world"
+*
+* @author Anastasia Pavlova (mailto:a-pavlova-1986@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
 package ru.job4j;
 public class Calculate {
   public static void main(String[] args) {
