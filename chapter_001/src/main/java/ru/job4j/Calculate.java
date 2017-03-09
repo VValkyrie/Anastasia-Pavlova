d076782f4858b7fb855b38b0d01b6dc374cec2fb
VValkyrie <a-pavlova-1986@yandex.ru>
2017-03-09 21:58:44 +0300

    add maven

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,4 @@
-﻿package ru.job4j;
+﻿//package ru.job4j;
 /**
 * //package for program "Hello, world"
 *
@@ -7,6 +7,7 @@
 * @since 0.1
 */
 
+package ru.job4j;
 public class Calculate {
   public static void main(String[] args) {
     System.out.println("Hello, world");
