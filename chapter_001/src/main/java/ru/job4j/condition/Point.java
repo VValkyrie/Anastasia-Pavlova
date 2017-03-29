a69b0beba8824ccd1f8bca6d912de9496dc685c8
VValkyrie <a-pavlova-1986@yandex.ru>
2017-03-29 17:23:08 +0300

    add maven

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -2,53 +2,60 @@ package ru.job4j.condition;
 
 /**.
  *package for program Point.
- *
  * @author Anastasia Pavlova (mailto: a-pavlova-1986@yandex.ru)
  * @version $Id$
  * since 0.1
  */
+
+/**.
+ * Класс Point описывает координаты точки в пространстве.
+ *@return результат.
+ */
+
 	public class Point {
 	/**.
-	*Класс Point описывает координаты точки в пространстве.
-	*/	
+	* Класс Point описывает координаты точки в пространстве.
+	*@param x - абсцисса.
+	*@param y - ордината.
+ 	*@return результат.
+	*/
 		private int x;
 		private int y;
 
-
+	/**.
+	 * конструктор Point описывает координаты точки.
+	 * @param x - координата точки по оси x.
+	 * @param y - координата точки по оси ую
+	 */
 	public Point(int x, int y) {
-		/**.
-        * конструктор Point описывает координаты точки.
-		* @param x - координата точки по оси x.
-		* @param y - координата точки по оси ую
-		*/
-
 		this.x = x;
 		this.y = y;
 	}
-	
+	/**.
+	 * Метод getX возвращает координату x.
+	 *@return - возвращает координату х.
+	 */
 	public int getX() {
-		/**.
-		* Метод getX возвращает координату x.
-		*@return - возвращает координату х.
-		*/
 		return this.x;
 	}
-	
+
+	/**.
+	 * Метод getX возвращает координату y.
+	 *@return - возвращает координату y.
+	 */
 	public int getY() {
-		/**.
-		* Метод getX возвращает координату y.
-		*@return - возвращает координату y.
-		*/
 		return this.y;
 	}
-	
+	/**.
+	 * Метод проверяет истинность
+	 * @return - возвращает значение у, результат.
+	 * @param a - кооэффициент a.
+	 * @param b - кооэффициент b.
+	 */
 	public boolean is(int a, int b) {
-		/**.
-		 * Метод проверяет истинность
-		 */
-    return this.y == a*x + b;
+    return this.y == a * x + b;
 		}
 
 	}
 
-	
+
