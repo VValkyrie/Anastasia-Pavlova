c6a33dcabe0ac04325911aebb8d4715f2e99fae8
VValkyrie <a-pavlova-1986@yandex.ru>
2017-03-31 15:45:30 +0300

    add maven

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Anastasia Pavlova (mailto:a-pavlova-1986@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class TriangleTest {
+    /**
+     * класс для тестирования калькулятора.
+     */
+    @Test
+
+/**.
+ * Метод, проверяющий принадлежность точки прямой.
+ */
+    public void forCheckAreaTriangle() {
+        Triangle abc = new Triangle();
+        abc.area(1, 3, 10, 10, 3, 3);
+        double result = 24.5;
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+}
+
